b40863b4833c0c313699005df639f7f1a20e1e82
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-22 10:31:46 +0100

    Functional Interfaces and Predicates

---
+++ src/com/sergio/Main.java
@@ -0,0 +1,88 @@
+package com.sergio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee john = new Employee("John Doe", 30);
+		Employee tim = new Employee("Tim Buchalka", 21);
+		Employee jack = new Employee("Jack Hill", 40);
+		Employee snow = new Employee("Snow White", 22);
+		Employee red = new Employee("Red Ridinghood", 35);
+		Employee charming = new Employee("Prince Charming", 31);
+
+		List<Employee> employees = new ArrayList<>();
+		employees.add(john);
+		employees.add(tim);
+		employees.add(jack);
+		employees.add(snow);
+		employees.add(red);
+		employees.add(charming);
+
+		printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
+		printEmployeesByAge(employees, "\nEmployees 30 and under", employee -> employee.getAge() <= 30);
+		printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
+			@Override
+			public boolean test(Employee employee) {
+				return employee.getAge() < 25;
+			}
+		});
+		
+		// each variable 'i' is within the scope of a single lambda
+		IntPredicate greaterThan15 = i -> i > 15;
+		IntPredicate lessThan100 = i -> i < 100;
+		
+		System.out.println(greaterThan15.test(10));
+		int a = 20;
+		System.out.println(greaterThan15.test(a+5));
+		
+		// chaining predicates
+		System.out.println(greaterThan15.and(lessThan100).test(50));
+		System.out.println(greaterThan15.and(lessThan100).test(15));
+		
+		Random random = new Random();
+		Supplier<Integer> randomSupplier = () -> random.nextInt(1000);	
+		for(int i=0; i<10;i++) {
+			System.out.println(randomSupplier.get());
+		}
+		
+	}
+
+	private static void printEmployeesByAge(List<Employee> employees, String ageText,
+			Predicate<Employee> ageCondition) {
+		System.out.println(ageText);
+		System.out.println("******");
+		for (Employee employee : employees) {
+			if (ageCondition.test(employee)) {
+				System.out.println(employee.getName());
+			}
+		}
+
+		// for(Employee employee : employees) {
+		// if(employee.getAge() > 30) {
+		// System.out.println(employee.getName());
+		// }
+		// }
+
+		// employees.forEach(employee -> {
+		// System.out.println(employee.getName());
+		// System.out.println(employee.getAge());
+		// });
+
+		// employees.forEach(new Consumer<Employee>() {
+		//
+		// @Override
+		// public void accept(Employee t) {
+		// System.out.println(t.getName());
+		// System.out.println(t.getAge());
+		// }
+		// });
+	}
+}
